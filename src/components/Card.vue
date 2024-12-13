91070a2835f4bba5516eeec68a6445d911e8e62b
ihorvyt <ihorvyt@gmail.com>
2024-12-13 17:55:19 +0200

    made html and tailwind

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+defineProps({
+  title: String,
+  img: String,
+  price: Number,
+});
+</script>
+
+<template>
+  <div
+    class="relative flex flex-col w-full border border-slate-100 rounded-xl p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2"
+  >
+    <div  class="absolute top-8 left-8">
+      <img :src="true ? '/like-1.svg' : '/like-2.svg'" alt="Favorite" />
+    </div>
+    <img :src="img" class="w-full" alt="Sneaker" />
+    <p>{{ title }}</p>
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col gap-2">
+        <span class="text-slate-200">Price:</span>
+        <span class="font-bold">${{ price }}</span>
+      </div>
+      <img src="/plus.svg" alt="Plus" />
+    </div>
+  </div>
+</template>
